020c5ef4f4472a9d86c6bfae358ab041c6c32344
Umesh Gohil <gohilumesh@hotmail.com>
2018-02-23 00:46:37 -0800

    Added the problem 55 solution

--- test/problems.js
+++ test/problems.js
@@ -1,5 +1,6 @@
 "use strict";
-let problems = require('./../lib/problems');
+let rewire = require('rewire');
+let problems = rewire('./../lib/problems');
 let expect = require("chai").expect;
 
 describe("Euler project Problems -", function() {
@@ -24,6 +25,63 @@ describe("Euler project Problems -", function() {
     it("should return correct result for 15 rows", () => {
       let result = problems.maxPathSum();
       expect(result).to.equal(1074);
+    });
+  });
+
+  describe("Lychrel numbers -", function() {
+    it("should return 0 result for 10", () => {
+      let result = problems.countLychrelNumbers(10);
+      expect(result).to.equal(0);
+    });
+
+    it("should return 0 result for 100", () => {
+      let result = problems.countLychrelNumbers(100);
+      expect(result).to.equal(0);
+    });
+
+    it("should return 1 result for 200", () => {
+      let result = problems.countLychrelNumbers(200);
+      expect(result).to.equal(1);
+    });
+  });
+
+  describe("isPalindromic -", function() {
+    it("should return true for palindromic numbers", () => {
+      let isPalindromic = problems.__get__("isPalindromic");
+      let result = isPalindromic(1);
+      expect(result).to.equal(true);
+
+      result = isPalindromic(222);
+      expect(result).to.equal(true);
+
+      result = isPalindromic(4444);
+      expect(result).to.equal(true);
+    });
+
+    it("should return false for palindromic numbers", () => {
+      let isPalindromic = problems.__get__("isPalindromic");
+      let result = isPalindromic(47);
+      expect(result).to.equal(false);
+
+      result = isPalindromic(123);
+      expect(result).to.equal(false);;
+    });
+  });
+
+  describe("isLychrel -", function() {
+    it("should return true for Lychrel numbers", () => {
+      let isLychrel = problems.__get__("isLychrel");
+      let result = isLychrel(196);
+      expect(result).to.equal(true);
+    });
+
+    it("should return false for Lychrel numbers", () => {
+      let isLychrel = problems.__get__("isLychrel");
+      let result = isLychrel(47);
+      expect(result).to.equal(false);
+
+      result = isLychrel(349);
+      expect(result).to.equal(false);;
     });
   });
 });
